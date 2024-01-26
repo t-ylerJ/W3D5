67e339535ca93a0532d97e9b6f54db2867be30f7
Tyler Johnson <tylerj0937@gmail.com>
2024-01-25 19:49:29 -0800

    phase 1 and 2 for  initialize and and parent

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,32 @@
+require "byebug"
+
 class PolyTreeNode
+  attr_reader :parent, :children, :value
   
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
+  
+  def parent=(parent)
+
+    if @parent != nil
+       @parent.children.select! { |child| child != self } 
+    end
+    @parent = parent 
+    @parent.children << self if !parent.nil?
+   
+  end
+
+
+
+
+ 
 end
+ 
+ test_node = PolyTreeNode.new(5)
+ parent_node = PolyTreeNode.new(10)
+#  test_node.parent = parent_node
+ p test_node
+
