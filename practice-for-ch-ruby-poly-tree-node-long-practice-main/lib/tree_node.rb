0397b4d8bcfe22c3e7c65ee6ed0b1455d99f6ba5
Caroline Zhang <cczhang1@yahoo.com>
2024-01-29 10:11:22 -0800

    Complete tree_node.rb

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -11,12 +11,22 @@ class PolyTreeNode
   
   # set current node(self) parent to a new parent
   def parent=(parent)
-
+    # tell parent that you're no longer part of it's children array, if you have a parent
+    # @parent.children.delete(self) if @parent
     if @parent != nil
        @parent.children.select! { |child| child != self } 
     end
+    
+    # set new parent of self
     @parent = parent 
-    @parent.children << self if !parent.nil?
+    # IMPORTANT NOTE: don't do this as an instance method to set the instance variable, you can set the instance variable 
+    # directly. Otherwise you will run an infinite loop, because you are currently creating an instance method of parent= here
+    # self.parent = new_parent 
+
+    # add yourself to that parent's children array (can only do this if you have a parent and you aren't already in that children array)
+    @parent.children << self if @parent && !@parent.children.include?(self)
+
+    # @parent.children << self if !parent.nil?
    
   end
 
@@ -39,9 +49,32 @@ class PolyTreeNode
   end
 
   def dfs(target_value)
-    return self.children[0] if self.children[0].value == target_value
-    self.dfs(target_value) 
+    # check to see if self has value
+    return self if self.value == target_value
+    
+    # if not self, move on to the children 
+    self.children.each do |child|
+      temp = child.dfs(target_value)
+      return temp if temp # by putting in return in loop, you can end loop early if you find value 
+    end
 
+    nil 
+
+    # OLD CODE 
+    # return self.children[0] if self.children[0].value == target_value
+    # self.dfs(target_value) 
+
+  end
+
+  # we want to use a queue for this 
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == target_value
+      queue += node.children
+    end 
+    nil
   end
 
 end
